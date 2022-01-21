de11b9104c385765bd7e3913e8e729f45074c184
Sumeyye ASLAN <sumeyye72.sa@gmail.com>
2022-01-21 13:09:13 +0300

    aaGame

---
+++ Assets/Scripts/CamControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+	void Update ()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        transform.position += (Vector3.up * ver + Vector3.right * hor) *Time.deltaTime;
+    }
+
+    
+}
---
+++ Assets/Scripts/moveCapsules.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class moveCapsules : MonoBehaviour
+{
+    public NavMeshAgent[] agent;
+    public GameObject target;
+
+    void OnMouseDown()
+    {
+        for(int i = 0; i< 8; i++)
+        {
+            agent[i].destination = target.transform.position;
+        }
+        
+        
+    }
+}
